767d6490d95f021b443f923896619b5f512c2423
vewe7 <naomi.yoon.do@gmail.com>
2023-03-24 20:02:26 -0400

    Add functionality for restarting the game and style changes

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const choiceStr = ['Rock', 'Paper', 'Scissors'];
 let playerScore = 0, computerScore = 0;
+let gameActive = true;
+
+const btns = document.querySelectorAll("button");
+const resDiv = document.querySelector(".result-box");
+const scoreDiv = document.querySelector(".score");
+
+const resetBtn = document.querySelector("#reset");
 
 // 0 - Rock; 1 - Paper; 2 - Scissors;
 function getComputerChoice() 
@@ -28,20 +35,33 @@ function printScore(playerScore, computerScore)
     console.log("You " + playerScore + " - Computer " + computerScore);
 }
 
+function updateResult(resDiv, resContent)
+{
+    let message = resDiv.removeChild(resDiv.firstChild);
+    message.textContent = resContent;
+    resDiv.appendChild(message);
+}
+
+function endGame(resetBtn)
+{
+    gameActive = false;
+    resetBtn.style.display = "inline-block";
+}
+
 function updateGame(res, player, computer, resDiv, scoreDiv)
 {
     if (res == -1)
     {
-        resDiv.textContent = "It's a Tie! " + choiceStr[player] + " against " + choiceStr[computer]; 
+        updateResult(resDiv, "It's a Tie! " + choiceStr[player] + " against " + choiceStr[computer]);
     }
     else if (res == 0)
     {
-        resDiv.textContent = "You Lose! " + choiceStr[computer] + " beats " + choiceStr[player];
+        updateResult(resDiv, "You Lose! " + choiceStr[computer] + " beats " + choiceStr[player]);
         computerScore++;
     }
     else 
     {
-        resDiv.textContent = "You Win! " + choiceStr[player] + " beats " + choiceStr[computer];
+        updateResult(resDiv, "You Win! " + choiceStr[player] + " beats " + choiceStr[computer]);
         playerScore++;
     }
 
@@ -49,24 +69,35 @@ function updateGame(res, player, computer, resDiv, scoreDiv)
 
     if (playerScore == 5)
     {
-        resDiv.textContent = "You win the game!"
+        updateResult(resDiv, "You win the game!");
+        endGame(resetBtn);
     }
     else if (computerScore == 5)
     {
-        resDiv.textContent = "You lose the game."
+        updateResult(resDiv, "You lose the game.");
+        endGame(resetBtn);
     }
 }
 
-const btns = document.querySelectorAll("button");
-const resDiv = document.querySelector(".result-message");
-const scoreDiv = document.querySelector(".score");
+
 
 btns.forEach(btn => {
     btn.addEventListener("click", () => {
-        let computerChoice = getComputerChoice(), playerChoice = parseInt(btn.dataset.choice);
-        let res = playRound(playerChoice, computerChoice);
+        if (gameActive)
+        {
+            let computerChoice = getComputerChoice(), playerChoice = parseInt(btn.dataset.choice);
+            let res = playRound(playerChoice, computerChoice);
 
-        updateGame(res, playerChoice, computerChoice, resDiv, scoreDiv)
+            updateGame(res, playerChoice, computerChoice, resDiv, scoreDiv)
+        }
     })
 });
 
+resetBtn.addEventListener("click", () => {
+    playerScore = 0, computerScore = 0;
+    updateResult(resDiv, "ROCK PAPER SCISSORS");
+    scoreDiv.textContent = "0 - 0";
+
+    gameActive = true;
+    resetBtn.style.display = "none";
+});
